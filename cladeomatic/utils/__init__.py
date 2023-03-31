c35c7c950acc6ea743e97d5745b49e7979c91775
eafreem <aisha.freeman@gmail.com>
2023-03-31 18:06:01 -0400

    Documentation update

--- cladeomatic/utils/__init__.py
+++ cladeomatic/utils/__init__.py
@@ -7,16 +7,9 @@ import pandas as pd
 
 def init_console_logger(lvl):
     """
-
-    Parameters
-    ----------
-    lvl [int] : Integer of level of logging desired 0,1,2,3
-
-    Returns
-    -------
-
-    logging object
-
+    This method initializes the logger for the console
+    :param lvl: int - level of logging desired 0,1,2,3
+    :return: logging - the logging object
     """
     logging_levels = [logging.ERROR, logging.WARN, logging.INFO, logging.DEBUG]
     report_lvl = logging_levels[lvl]
@@ -26,6 +19,12 @@ def init_console_logger(lvl):
 
 
 def run_command(command):
+    """
+    This method runs the passed command on the shell command line
+    :param command: String - the command for the command line
+    :return: bytes - stdout, stderr: the standard out and error messages
+    returned by the command line
+    """
     p = Popen(command, shell=True, stdout=PIPE, stderr=PIPE)
     stdout, stderr = p.communicate()
     stdout = stdout.decode('utf-8')
@@ -33,6 +32,13 @@ def run_command(command):
     return stdout, stderr
 
 def calc_shanon_entropy(value_list):
+    """
+    This method calculates the shannon entropy value for the list
+    of numbers passed
+    :param value_list: list - the list of values to use for the calculation
+    :return: float - the calculated Shannon entropy or -1 if there are no
+    values in the passed list
+    """
     total = sum(value_list)
     if total == 0:
         return -1
@@ -42,17 +48,33 @@ def calc_shanon_entropy(value_list):
     return entropy(values)
 
 def calc_AMI(category_1, category_2):
+    """
+    Calculates the adjusted mutual info score between two clusterings
+    :param category_1: list - a list of int values for cluster 1
+    :param category_2: list - a list of int values for cluster 2
+    :return: float - the value of the AMI score for the two clusters
+    """
     return adjusted_mutual_info_score(category_1, category_2, average_method='arithmetic')
 
 def calc_ARI(category_1, category_2):
+    """
+    Calculates the adjusted rand score between two clusterings
+    :param category_1: list - a list of int values for cluster 1
+    :param category_2: list - a list of int values for cluster 2
+    :return: float - the value of the ARI score for the two clusters
+    """
     return adjusted_rand_score(category_1, category_2)
 
 def parse_metadata(file):
-    '''
-    Parses metadata file into a dict with samples as the keys
-    :param file: str path to metdata file with 'sample_id' as a colum
-    :return: dict
-    '''
+    """
+    Parses the metadata file into a dictionary with sample ids as the keys.
+    Will dynamically add the other columns and values to the dictionary
+    :param file: string - path to tsv metadata file with 'sample_id' as a
+    mandatory column
+    :return: dictionary - a dictionary of the parsed metadata values organized
+    wtih the sample id as a key
+    """
+    #read the file into a pandas datafram
     df = pd.read_csv(file, sep="\t", header=0)
     if not 'sample_id' in df.columns.tolist():
         return {}
@@ -60,6 +82,7 @@ def parse_metadata(file):
     metadata = {}
     for index, row in df.iterrows():
         metadata[row['sample_id']] = {}
+        #parse out any additional columns and their row values
         for field in columns:
             field = str(field)
             if field == 'sample_id':
